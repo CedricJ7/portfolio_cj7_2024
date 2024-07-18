ec7597e112fc5dd388d327bed4d6f3701b02436c
Cedric Jestin <cedricj0712@gmail.com>
2024-07-18 21:25:53 +0200

    add content

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio Cédric Jestin 2024",
+  description: "Modern and minimalist portfolio with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}> 
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+        {children}
+        </ThemeProvider></body>
+    </html>
+  );
+}
